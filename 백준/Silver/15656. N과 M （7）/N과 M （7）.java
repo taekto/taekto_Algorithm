94d2512e785ec6cc142f66f0d93d785b8753aa8f
taekto <110243496+taekto@users.noreply.github.com>
2024-05-01 14:00:59 +0900

    [Silver III] Title: N과 M (7), Time: 656 ms, Memory: 220096 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr;
+    static int[] ans;
+    static boolean[] visited;
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+    static void func(int depth) {
+        if(depth == M) {
+            for(int i=0;i<M;i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0;i<N;i++) {
+            ans[depth] = arr[i];
+            func(depth+1);
+            ans[depth] = 0;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        ans = new int[M];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        func(0);
+        System.out.println(sb.toString());
+    }
+}
